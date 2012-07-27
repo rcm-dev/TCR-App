88108001be04b5754db6f7a0a4b895d975333d6a
richcoremedia <mahfudz@richcoremedia.com>
2012-07-27 11:52:18 +0800

    + Added two version live and localhost

--- Connections/conRCMS.php
+++ Connections/conRCMS.php
@@ -1,10 +1,20 @@
 <?php
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
-# HTTP="true"
-$hostname_conRCMS = "localhost";
+# HTTP="true"\
+
+#localhost
+/*$hostname_conRCMS = "localhost";
 $database_conRCMS = "tcr_app";
 $username_conRCMS = "root";
-$password_conRCMS = "";
+$password_conRCMS = "";*/
+
+#live richcoremedia
+$hostname_conRCMS = "localhost";
+$database_conRCMS = "richcore_tcrapp";
+$username_conRCMS = "richcore_tcruser";
+$password_conRCMS = "tcrpassword";
+
+
 $conRCMS = mysql_pconnect($hostname_conRCMS, $username_conRCMS, $password_conRCMS) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
